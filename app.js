fc9f636ae700ba3e440e500b7f68bb095cd820e5
sabriVorpagel <sabrivorpagel2512@gmail.com>
2022-08-02 23:14:04 -0300

    añado heroku

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+require('dotenv').config()
 const express = require("express");
 const path = require('path');
 const app = express();
-const port = 3030;
+
 
 /*recursos estaticos*/
 
 app.use(express.static('public'));
+app.set('puerto', process.env.PORT || 3001);
 
 /*rutas*/
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')));
 app.get('/productdetail', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html')));
 app.get('/productCart', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productCart.html')));
 app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 
 /*puerto*/
-app.listen(port, () => console.log("Server running in http://localhost:" + port))
+app.listen(process.env.PORT || 3000, function() { 
+    console.log('Servidor corriendo en el puesto 3030')
+})
 
