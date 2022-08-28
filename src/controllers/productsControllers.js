8e1c9c9017617a557e0e7a987b8c2d782165c900
SGOviedo <oviedosantiagogabriel@gmail.com>
2022-08-28 19:36:07 -0300

    problemas

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -4,7 +4,6 @@ const path = require('path');
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = {
-
     product: (req, res) => { 
         const products = loadProducts();
         return res.render('products/product', {
@@ -23,11 +22,7 @@ module.exports = {
         return res.render('products/productCart')
     },
     edit: (req, res) => {
-<<<<<<< HEAD
-        return res.render('products/registerEdit')
-=======
         return res.render('products/productEdit')
->>>>>>> 3f24ad92c7a2fac6565aeb3bbcf4fa6a634bedc5
     },
     create: (req, res) => {
         return res.render('products/productCreate')
