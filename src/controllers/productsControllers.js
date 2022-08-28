59b6d09d485ade70b9a50e97ac8a9d11792f2d04
SGOviedo <oviedosantiagogabriel@gmail.com>
2022-08-28 20:53:01 -0300

    temporal

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -22,11 +22,57 @@ module.exports = {
         return res.render('products/productCart')
     },
     edit: (req, res) => {
-        return res.render('products/productEdit')
+        let productToEdit = loadProducts().find(product => product.id === +req.params.id);
+		return res.render('products/productEdit', {
+			productToEdit
+		})
     },
+    // Create - Form to create
     create: (req, res) => {
         return res.render('products/productCreate')
-    }
+    },
+    // Create -  Method to store
+    store: (req,res) =>{
+        const {name, price, discount, description, category, subCategory} = req.body;
+		let products = loadProducts();
+		const newProduct = {
+			id: products[products.length -1].id +1,
+			...req.body,
+			name: name,
+			price: +price,
+			description: description,
+			discount: +discount,
+			category,
+            subCategory,
+			image: 'kit-collar.jpeg'
+		}
+        let productsModify = [...products, newProduct];
+		storeProducts(productsModify);
+
+		return res.redirect('/')
+    },
+    update: (req, res) => {
+		// Do the magic
+		const {name, price, discount, description, category, subCategory, stock} = req.body;
+		let productsModify = loadProducts().map(product => {
+			if(product.id === +req.params.id){
+				return {
+					id: products.id,
+					name: name.trim(),
+					price: +price,
+					description: description.trim(),
+					discount: +discount,
+                    stock: +stock,
+					category,
+                    subCategory,
+					image: product.image
+				}
+			}
+			return product
+		})
+		storeProducts(productsModify)
+		return res.redirect('/products/product' + req.params.id )
+	},
 }
 
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const { product,detail, cart, create, edit} = require('../controllers/productsControllers');
+const { product,detail, cart, create, edit, store, update} = require('../controllers/productsControllers');
 
 /* /products */
 
 router
     .get('/product', product)
+    /*** GET ONE PRODUCT ***/ 
     .get('/detail/:id', detail)
+    /*** GET CART ***/ 
     .get('/cart', cart)
+    /*** CREATE ONE PRODUCT ***/
     .get('/create', create)
-    .get('/edit', edit)
+    .post('/store', store)
+    /*** EDIT ONE PRODUCT ***/ 
+    .get('/edit/:id', edit)
+    .put('/update/:id', update)
+    /*** DELETE ONE PRODUCT***/ 
 
 module.exports = router;
