1b1438879576af3dcdbfd9d237c39fb64aa25193
sabriVorpagel <sabrivorpagel2512@gmail.com>
2022-12-01 19:32:07 -0300

    list and detail the productsApis

--- src/controllers/api/apiProductsController.js
+++ src/controllers/api/apiProductsController.js
@@ -1,1 +1,94 @@
-//apis products sabri
+const db = require('../../database/models');
+const path = require('path');
+const {loadProducts, storeProducts}= require('../../data/db_Module');
+//const{sendSequelizeError}= require('../../helpers')
+const  {literal} = require('sequelize');
+
+module.exports = {
+    list: async (req, res) => {
+        try {
+            let {limit} = req.query; 
+            let options = {
+                attributes : {
+                    exclude : ['createdAt', 'updatedAt', 'delatedAt'],
+                    include : [ [literal(`CONCAT('${req.protocol}://${req.get('host')}/products/',Product.id)`), 'url']]
+                },
+                include : [
+                    {
+                        association : 'images',
+                        attributes : {
+                            exclude : ['createdAt', 'updateAt', 'deleteAt', 'id', 'file', 'productId'],
+                            include : [[literal(`CONCAT('${req.protocol}://${req.get('host')}/products/image',file)`), 'url']]
+                        }
+                    },
+                    {
+                        association :'category',
+                        attributes : ['name'],
+                    }
+                ],
+                limit : +limit || 4
+            }
+            const {count, rows : products} = await db.Product.findAndCountAll(options);
+
+            return res.status(200).json({
+                ok: true, 
+                meta : {
+                    total : count,
+                    quantity : products.length
+                },
+                data: products  
+            
+            })
+            // de donde viene el products de data
+        
+        } catch (error) {
+            let errors = sendSequelizeError(error);
+            
+            return res.status(error.status || 500).json ({
+                ok: false,
+                errors,
+            });
+        }
+    },
+    //Detalle del producto
+    detail: async (req, res) => {
+        try {
+            const {id}= req.params;
+            const options = {
+                include : [
+                    {
+                        association : 'images',
+                        attributes : {
+                            exclude : ['createdAt', 'updateAt', 'deleteAt', 'id', 'file', 'productId'],
+                            include : [[literal(`CONCAT('${req.protocol}://${req.get('host')}/products/image',file)`), 'imageURL']]
+                        }
+                    },
+                    {
+                        association : 'category',
+                        attributes :['name']
+                    }
+                ]
+            }
+            const product = await db.Product.findByPk( id, options);
+
+            return res.status(200).json({
+                ok: true, 
+                data: product  
+            
+            })
+        } catch (error) {
+            let errors = sendSequelizeError(error);
+            
+            return res.status(error.status || 500).json ({
+                ok: false,
+                errors,
+            });
+            
+        }
+        
+    },
+
+    getImage : async (req, res) => {
+        return res.sendFile(path.join(__dirname, '..','..','public','images','products', req.params.image))
+    }
+}
---
+++ src/routes/api/apiProductsRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const {list, detail, getImage } = require('../../controllers/api/apiProductsController')
+
+router
+    .get('/list', list)
+    /*** GET ONE PRODUCT ***/ 
+    .get('/detail/:id', detail)
+    /***GET IMAGES ***/
+    .get('/image/:image', getImage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
