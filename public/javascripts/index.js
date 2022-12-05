dd07f33a73f8fefaa9817e39c100520293ef1c42
VeroVelazco <veroverona31@gmail.com>
2022-12-04 23:31:53 -0300

    agregando boostrap

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,28 +1,45 @@
-windows.addEventListener("load", function(){
-    let menu = document.querySelector("#menu");
+Swal.fire({
+    title: 'Do you want to save the changes?',
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: '',
+    denyButtonText: `Don't save`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Swal.fire('Saved!', '', 'success')
+    } else if (result.isDenied) {
+      Swal.fire('Changes are not saved', '', 'info')
+    }
+  })
 
-    menu.addEventListener("submit", function(e){
-        
-    })
-})
 
-.onload = () => {
-        Swal.fire({
-            title: 'Do you want to save the changes?',
-            showDenyButton: true,
-            showCancelButton: true,
-            confirmButtonText: 'Save',
-            denyButtonText: `Don't save`,
-          }).then((result) => {
-            /* Read more about isConfirmed, isDenied below */
-            if (result.isConfirmed) {
-              Swal.fire('Saved!', '', 'success')
-            } else if (result.isDenied) {
-              Swal.fire('Changes are not saved', '', 'info')
-            }
-          })
+  let form = document.querySelector('submit', function (e){
+    e.form.addEventListener('')
   
-}
+  
+  
+  
+  })
+  
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+  Toast.fire({
+    icon: 'success',
+    title: 'Signed in successfully'
+  })
+  
+
 
 
 
