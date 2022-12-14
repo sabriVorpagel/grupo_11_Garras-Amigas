fb2eb59189ccef10cb5831dc73ba580d36933c08
VeroVelazco <veroverona31@gmail.com>
2022-12-14 12:39:03 -0300

    subiendo cambios

---
+++ garrasamigas/src/components/SideBar.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+export const SideBar = () =>{
+    return(
+        <React.Fragment>
+            {/*<!-- Sidebar -->*/}
+            <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+                {/*<!-- Sidebar - Brand -->*/}
+                <a className="sidebar-brand d-flex" href="/">
+                    <div className="sidebar-brand-icon">
+                        <img className="w-100" alt="Garras Amigas"/>
+                    </div>
+                </a>
+
+                {/*<!-- Divider -->*/}
+                <hr className="sidebar-divider my-0"/>
+
+                {/*<!-- Nav Item - Garras Amigas -->*/}
+                <li className="nav-item active">
+                    <a className="nav-link" href="/">
+                        <i className="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Administarción</span></a>
+                </li>
+
+                {/*<!-- Divider -->*/}
+                <hr className="sidebar-divider"/>
+
+                {/*<!-- Heading -->*/}
+                <div className="sidebar-heading">Navegación</div>
+
+                {/*<!-- Nav Item - Pages -->*/}
+                <li className="nav-item">
+                    <a className="nav-link collapsed" href="/">
+                        <i className="fas fa-fw fa-folder"></i>
+                        <span>Productos</span>
+                    </a>
+                </li>
+
+                {/*<!-- Nav Item - Charts -->*/}
+                <li className="nav-item">
+                    <a className="nav-link" href="/">
+                        <i className="fas fa-fw fa-chart-area"></i>
+                        <span>Usuarios</span></a>
+                </li>
+
+                {/*<!-- Nav Item - Tables -->*/}
+                <li className="nav-item">
+                    <a className="nav-link" href="/">
+                        <i className="fas fa-fw fa-table"></i>
+                        <span>Categorias</span></a>
+                </li>
+
+                {/*<!-- Divider -->*/}
+                <hr className="sidebar-divider d-none d-md-block"/>
+            </ul>
+            {/*<!-- End of Sidebar -->*/}
+            
+        </React.Fragment>
+    )
+}
+
