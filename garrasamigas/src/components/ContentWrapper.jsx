f9358132bced77a0441a5431acb19b535b1477c6
VeroVelazco <veroverona31@gmail.com>
2022-12-15 16:18:31 -0300

    subo cambios a master

--- garrasamigas/src/components/ContentWrapper.jsx
+++ garrasamigas/src/components/ContentWrapper.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { MetricAll } from './MetricAll'
+import { Footer } from './Footer'
+import { TopBar } from './TopBar'
 
 export const ContentWrapper = () => {
   return (
-    <div>ContentWrapper</div>
+   <React.Fragment>
+            {/*<!-- Content Wrapper -->*/}
+            <div id="content-wrapper" className="d-flex flex-column">
+                {/*<!-- Main Content -->*/}
+                <div id="content">
+                <TopBar />
+                <MetricAll />
+                   
+                </div>
+                <Footer />
+            </div>    
+        </React.Fragment>
   )
 }
