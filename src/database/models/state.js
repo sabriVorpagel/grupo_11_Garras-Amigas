c34e86b31044289ffd6fd811dc2bafad340ed71b
Eric <menaericdaniel@gmail.com>
2023-01-02 08:03:56 -0300

    resolve carts

--- src/database/models/state.js
+++ src/database/models/state.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.Order,{
+        as : 'order',
+        foreignKey : 'statusesId'
+      })
     }
   }
   State.init({
